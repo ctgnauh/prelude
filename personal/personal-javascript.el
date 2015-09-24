6745adb00245ad1dc07f43b55288745aa0117eb6
ctgnauh <huangtc@outlook.com>
2015-09-24 13:08:09 +0800

    使用tern.js代替js-ac2

--- personal/personal-javascript.el
+++ personal/personal-javascript.el
@@ -3,19 +3,19 @@
 ;;; Code:
 
 ;; ac-js2
-(prelude-require-package 'ac-js2)
-(add-hook 'js2-mode-hook 'ac-js2-mode)
+;; (prelude-require-package 'ac-js2)
+;; (add-hook 'js2-mode-hook 'ac-js2-mode)
 
 ;; ternjs
-;; (prelude-require-packages '(tern tern-auto-complete))
-;; (add-hook 'js2-mode-hook (lambda ()
-;;                            (auto-complete-mode)
-;;                            (tern-mode t)))
-;; (setq tern-command (cons (executable-find "tern") '()))
-;; (eval-after-load 'tern
-;;   '(progn
-;;      (require 'tern-auto-complete)
-;;      (tern-ac-setup)))
+(prelude-require-packages '(tern tern-auto-complete))
+(add-hook 'js2-mode-hook (lambda ()
+                           (auto-complete-mode)
+                           (tern-mode t)))
+(setq tern-command (cons (executable-find "tern") '()))
+(eval-after-load 'tern
+  '(progn
+     (require 'tern-auto-complete)
+     (tern-ac-setup)))
 
 (provide 'personal-javascript)
 ;;; personal-javascript.el ends here
